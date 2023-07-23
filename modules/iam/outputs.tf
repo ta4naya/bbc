665d10c776e751b0b05df0a6f3d93afdb28fce82
ta4naya <ta4naya@yahoo.com>
2023-07-23 14:48:51 +0200

    Delete modules directory
    bbb

--- modules/iam/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "instance_profile_bastion_name" {
-  value = aws_iam_instance_profile.bastion.name
-}
-
-output "instance_profile_private_name" {
-  value = aws_iam_instance_profile.private.name
-}
--- modules/route53/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "public_zone_id" {
-  value = aws_route53_zone.bdpa_public.id
-}
-
-output "private_zone_id" {
-  value = aws_route53_zone.bdpa_private.id
-}
